2402d0bd6d68f87274c08fe8fd6ae499e7ff85a9
Christoph <christoph.hannebauer@glueckkanja-gab.com>
2023-07-18 12:12:43 +0200

    typo in comment

--- SCEPman/Private/permissions.ps1
+++ SCEPman/Private/permissions.ps1
@@ -46,7 +46,7 @@ function GetSCEPmanResourcePermissions() {
     $intuneResourceId = GetAzureResourceAppId -appId $IntuneAppId
 
     ### Managed identity permissions for SCEPman
-    if ($null -eq $intuneResourceId) {  # When not using Intune at all (e.g. only JAMF), there is IntuneAppId can be $null
+    if ($null -eq $intuneResourceId) {  # When not using Intune at all (e.g. only JAMF), the IntuneAppId can be $null
         return @([pscustomobject]@{'resourceId'=$graphResourceId;'appRoleId'=$MSGraphDirectoryReadAllPermission;},
                 [pscustomobject]@{'resourceId'=$graphResourceId;'appRoleId'=$MSGraphDeviceManagementReadPermission;},
                 [pscustomobject]@{'resourceId'=$graphResourceId;'appRoleId'=$MSGraphDeviceManagementConfigurationReadAll;},
