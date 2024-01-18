07740d55ccdbccdb7f38a0a9ec354c3b30fd746e
Christoph <christoph.hannebauer@glueckkanja-gab.com>
2024-01-18 15:41:06 +0100

    passed correct object on az authorization

--- SCEPman/Private/appregistrations.ps1
+++ SCEPman/Private/appregistrations.ps1
@@ -132,7 +132,7 @@ function Add-AzAsTrustedClientApplication ($AppId) {
       $existingAzAuthorization = [System.Collections.ArrayList]@()
     }
     $null = $existingAzAuthorization.Add(@{ 'appId' = $AzAppId; 'permissionIds' = @($AccessApplicationPermissionId) })
-    $previousPreAuthorizationsInner = ConvertTo-Json -InputObject $authorizationsWithoutAz -Compress
+    $previousPreAuthorizationsInner = ConvertTo-Json -InputObject $existingAzAuthorization -Compress
     $previousPreAuthorizationsBody = "{'api':{'preAuthorizedApplications':$($previousPreAuthorizationsInner.Replace("delegatedPermissionIds", "permissionIds").Replace('"', "'"))}}"
 
     $null = ExecuteAzCommandRobustly -callAzNatively $true -azCommand @('rest', '--method', 'patch', '--uri', "https://graph.microsoft.com/beta/applications/$($AppObject.id)", '--body', $previousPreAuthorizationsBody, '--headers', 'Content-Type=application/json')
