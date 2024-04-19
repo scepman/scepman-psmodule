d5d1e6226652df2b14526ca758fb577e6e3eed87
Christoph <christoph.hannebauer@glueckkanja-gab.com>
2024-04-19 11:20:36 +0200

    #51 Native az call

--- SCEPman/Private/key-vault.ps1
+++ SCEPman/Private/key-vault.ps1
@@ -44,7 +44,8 @@ function New-IntermediateCaCsr {
     #az keyvault certificate create --policy @C:\temp\certs\keyvault\rsa-policy.json --vault-name $vaultName --name $certificateName
 
       # The direct graph call instead works
-    $creationResponseLines = ExecuteAzCommandRobustly -azCommand "az rest --method post --uri $($vaultUrl)certificates/$certificateName/create?api-version=7.0 --headers 'Content-Type=application/json' --resource $vaultDomain --body '$caPolicyJson'"
+    $creationResponseLines = ExecuteAzCommandRobustly -azCommand @("rest", "--method", "post", "--uri", "$($vaultUrl)certificates/$certificateName/create?api-version=7.0", 
+    "--headers", "'Content-Type=application/json'", "--resource", $vaultDomain, "--body", $caPolicyJson) -callAzNatively
     $creationResponse = Convert-LinesToObject -lines $creationResponseLines
 
     Write-Information "Created a CSR with Request ID $($creationResponse.request_id)"
