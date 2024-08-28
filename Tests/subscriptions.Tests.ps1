0cdd87ec4cbaf543ac9a23aab935ffed2311eedc
Ben Godwin <bengodw@gmail.com>
2024-08-28 17:22:50 +1000

    wrote subscriptions and constants tests

--- Tests/subscriptions.Tests.ps1
+++ Tests/subscriptions.Tests.ps1
@@ -9,12 +9,12 @@ Describe 'Geos' {
         Mock Invoke-Az {
             param($azCommand)
 
-            if ($azCommand[0] -ne 'account' -or $azCommand[1] -ne 'list-locations')
+            if ($azCommand[0] -eq 'account' -and $azCommand[1] -eq 'list-locations') 
             {
-                throw "Unexpected command: $azCommand"
+                return Get-Content -Path "./Tests/Data/locations.json"
             }
 
-            return Get-Content -Path "./Tests/Data/locations.json"
+            throw "Unexpected command: $azCommand"
         }
     }
 
@@ -29,4 +29,56 @@ Describe 'Geos' {
 
         $result | Should -Be $false
     }
+
+}
+
+Describe 'GetResources' {
+    BeforeAll {
+        Mock Invoke-Az {
+            param($Command)
+
+            if ($Command[0] -eq "graph" -and $Command[1] -eq "query")
+            {
+                return Get-Content -Path "./Tests/Data/subscriptions.json"
+            } 
+
+            throw "Unexpected command: $Command"
+        }
+    }
+
+    It 'Can successfully get matching subscription details' {
+        $subscriptionsJson = Get-Content -Path "./Tests/Data/accounts.json"
+        $subscriptions = Convert-LinesToObject $subscriptionsJson
+        $result = GetSubscriptionDetailsUsingAppName -AppServiceName "app-service-name" -subscriptions $subscriptions
+
+        $expected = $subscriptions[0], $subscriptions[1]
+        $result | Should -Be $expected
+    }
+
+    
+    It 'Will throw an error if no matching subscription exists from graph' {
+        $subscriptionsJson = Get-Content -Path "./Tests/Data/accounts-no-match.json"
+        $subscriptions = Convert-LinesToObject $subscriptionsJson
+        { GetSubscriptionDetailsUsingAppName -AppServiceName "app-service-name" -subscriptions $subscriptions } | Should -Throw
+    }
+
+    It 'Will throw an error if graph query has no output' {
+        Mock Invoke-Az {
+            param($Command)
+
+            if ($Command[0] -eq "graph" -and $Command[1] -eq "query")
+            {
+                return $null
+            } 
+
+            throw "Unexpected command: $Command"
+        }
+
+        $subscriptionsJson = Get-Content -Path "./Tests/Data/accounts.json"
+        $subscriptions = Convert-LinesToObject $subscriptionsJson
+        { GetSubscriptionDetailsUsingAppName -AppServiceName "app-service-name" -subscriptions $subscriptions } | Should -Throw
+
+    }
+
+
 }
