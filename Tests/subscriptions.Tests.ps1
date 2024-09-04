c903b70d639ff0a1cf2bce0e8316a5173497be8b
Ben Godwin <bengodw@gmail.com>
2024-09-04 14:56:25 +1000

    GetSubscriptionDetails tests

--- Tests/subscriptions.Tests.ps1
+++ Tests/subscriptions.Tests.ps1
@@ -32,7 +32,7 @@ Describe 'Geos' {
 
 }
 
-Describe 'GetResources' {
+Describe 'GetSubscriptionDetailsUsingAppName' {
     BeforeAll {
         Mock Invoke-Az {
             param($Command)
@@ -51,12 +51,12 @@ Describe 'GetResources' {
         $subscriptions = Convert-LinesToObject $subscriptionsJson
         $result = GetSubscriptionDetailsUsingAppName -AppServiceName "app-service-name" -subscriptions $subscriptions
 
-        $expected = $subscriptions[0], $subscriptions[1]
+        $expected = $subscriptions[0]
         $result | Should -Be $expected
     }
 
     
-    It 'Will throw an error if no matching subscription exists from graph' {
+    It 'Will throw an error if no matching subscription exists in accounts' {
         $subscriptionsJson = Get-Content -Path "./Tests/Data/accounts-no-match.json"
         $subscriptions = Convert-LinesToObject $subscriptionsJson
         { GetSubscriptionDetailsUsingAppName -AppServiceName "app-service-name" -subscriptions $subscriptions } | Should -Throw
@@ -82,3 +82,90 @@ Describe 'GetResources' {
 
 
 }
+
+Describe 'GetSubscriptionDetails' {
+    BeforeAll {
+        Mock az {
+            param($Command)
+
+            if ($Command -eq "account")
+            {
+                return Get-Content -Path "./Tests/Data/accounts.json"
+            } 
+            elseif ($Command -eq "graph")
+            {
+                return Get-Content -Path "./Tests/Data/subscriptions.json"
+            }
+
+            throw "Unexpected command: $Command"
+        }
+
+        Mock Invoke-Az {
+            param($Command)
+
+            if ($Command[0] -eq "graph" -and $Command[1] -eq "query")
+            {
+                return Get-Content -Path "./Tests/Data/subscriptions.json"
+            }
+
+            throw "Unexpected command: $Command"
+        }
+    }
+
+    It 'Can successfully get matching subscription details' {
+        $result = GetSubscriptionDetails -SubscriptionId "subscription-id"
+        $subscriptionsJson = Get-Content -Path "./Tests/Data/accounts.json"
+        $subscriptions = Convert-LinesToObject $subscriptionsJson
+        
+        $expected = $subscriptions[0]
+        # TODO: Work out why pester doesn't think these two objects are equal when they clearly are (without ["id"])
+        $result["id"] | Should -Be $expected["id"]
+    }
+
+    It 'Lets you select a subscription with the app service name flag' {
+        $result = GetSubscriptionDetails -SearchAllSubscriptions 1 -AppServiceName app-service-name
+        $subscriptionsJson = Get-Content -Path "./Tests/Data/accounts.json"
+        $subscriptions = Convert-LinesToObject $subscriptionsJson
+        
+        $expected = $subscriptions[0]
+        $result["id"] | Should -Be $expected["id"]
+    }
+
+    It 'Lets you select a subscription with the app service plan name flag' {
+        $result = GetSubscriptionDetails -SearchAllSubscriptions 1 -AppServicePlanName app-service-plan-name
+        $subscriptionsJson = Get-Content -Path "./Tests/Data/accounts.json"
+        $subscriptions = Convert-LinesToObject $subscriptionsJson
+        
+        $expected = $subscriptions[0] 
+        $result["id"] | Should -Be $expected["id"]
+    }
+
+    It 'Will return a single subscription if only 1 exists' {
+        Mock az {
+            param($Command)
+
+            if ($Command -eq "account")
+            {
+                return Get-Content -Path "./Tests/Data/accounts-no-match.json"
+            } 
+        }
+        $result = GetSubscriptionDetails -SearchAllSubscriptions 1
+        $subscriptionsJson = Get-Content -Path "./Tests/Data/accounts-no-match.json"
+        $subscriptions = Convert-LinesToObject $subscriptionsJson
+        
+        $expected = $subscriptions[0] 
+        $result["id"] | Should -Be $expected["id"]
+
+    }
+
+    It 'Will throw an error if no matching subscription exists in accounts' {
+        { GetSubscriptionDetails -SubscriptionId "incorrect-subscription-id" } | Should -Throw
+    }
+
+    It 'Will throw an error if no id, app service name or app service plan name (and multiple subscriptions)' {
+        { GetSubscriptionDetails -SearchAllSubscriptions 1 } | Should -Throw
+    }
+    
+    # TODO: how to simulate user input inline??
+    # Write-Output 1 | GetSubscriptionDetails
+} 
