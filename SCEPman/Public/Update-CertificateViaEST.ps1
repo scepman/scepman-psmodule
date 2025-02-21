2c3c69d5c4479dbeed0d7faede5e83f0ff2e129d
Constantin Heinzler <constantin.heinzler@glueckkanja.com>
2025-02-21 13:57:09 +0100

    Remove PoSh version check

--- SCEPman/Public/Update-CertificateViaEST.ps1
+++ SCEPman/Public/Update-CertificateViaEST.ps1
@@ -56,9 +56,6 @@ Function Update-CertificateViaEST {
         [switch]$AllowInvalid
     )
     BEGIN {
-        if ($PSVersionTable.PSVersion.Major -lt 7) {
-            throw "This script requires PowerShell 7 or higher."
-        }
 
         if([System.Environment]::OSVersion.Platform -ne 'Win32NT') {
             throw "EST Renewal with this CMDlet is only supported on Windows. For Linux, use EST with another tool like this sample script: https://github.com/scepman/csr-request/blob/main/enroll-certificate/renewcertificate.sh"
