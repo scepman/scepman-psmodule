0819d349f027db34f871e12a1ab75ea940e79502
Constantin Heinzler <constantin.heinzler@glueckkanja.com>
2026-01-28 11:36:29 +0100

    Assign DCR permissions if configured

--- SCEPman/Public/New-SCEPmanClone.ps1
+++ SCEPman/Public/New-SCEPmanClone.ps1
@@ -133,6 +133,14 @@ function New-SCEPmanClone
 
         Write-Information "Adding permissions to Key Vault"
         AddSCEPmanPermissionsToKeyVault -KeyVault $keyvault -PrincipalId $serviceprincipalsc.principalId
+
+        $DcrId = $SCEPmanSourceSettings.settings | Where-Object name -match 'AppConfig(:|__)LoggingConfig(:|__)RuleId' | Select-Object -ExpandProperty value
+        if ($DcrId) {
+            Write-Information "Found configured data collection rule. Confirm Data Collection rule and find resource id"
+            $DataCollectionRule = GetDataCollectionRule -DcrId $DcrId
+
+            AddAppRoleAssignmentsForLogIngestionAPI -ResourceGroup $TargetResourceGroup -AppServiceName $TargetAppServiceName -DcrId $DataCollectionRule.id
+        }
     }
 
     if ($null -ne $scepManVnetId) {
